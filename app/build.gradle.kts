bd1e14a480a2a1985d49f61322a363a901bfccfd
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-08-15 10:55:57 -0300

    Add Color Cloud projection renderer
    * Add Color Cloud projection renderer
    
    * Bump Color Cloud prototype to v0.12
    
    * Enable CI for Color Cloud branch
    
    * Restore CI trigger to main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bwa3d.ambiprojector"
         minSdk = 23
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.11.0"
+        versionCode = 12
+        versionName = "0.12.0"
     }
 
     buildTypes {
