05e5451271fe9481d74067a90bb20744a9a15117
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-08-15 13:07:00 -0300

    Bump projector to v0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bwa3d.ambiprojector"
         minSdk = 23
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.17.0"
+        versionCode = 18
+        versionName = "0.18.0"
     }
 
     buildTypes {
