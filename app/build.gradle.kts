4a3bcc711964e399a595c31ef08535545f180d41
diegocesaretti <47791066+diegocesaretti@users.noreply.github.com>
2026-08-15 22:28:48 -0300

    Bump Mi Box GPU projector to v0.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.bwa3d.ambiprojector.mibox"
         minSdk = 23
         targetSdk = 28
-        versionCode = 25
-        versionName = "0.25.0-mibox9"
+        versionCode = 26
+        versionName = "0.26.0-mibox9-gpu"
     }
 
     buildTypes {
